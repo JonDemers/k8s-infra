66d9d6950944b95a995b96762bae3c69e0964520
Jonathan Demers <jdemers@opcodesolutions.com>
2022-12-23 10:39:15 -0500

    Consolidate OIDC infrastructure and simplify config

--- build-single.sh
+++ build-single.sh
@@ -14,12 +14,15 @@ cd $(dirname "$0")/$name
 if [ -f "Dockerfile" ]; then
   docker_name=com.opcode.k8s-infra.$name
   docker build --tag $docker_name .
-  export DOCKER_IMAGE=debian-cloud-test:8083/$docker_name:latest-$(date "+%Y.%m.%d-%H.%M.%S")
-  docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
+  export DOCKER_IMAGE=docker-registry.k8s.lan/$docker_name:latest-$(date "+%Y.%m.%d-%H.%M.%S")
+#  docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
   docker tag $docker_name $DOCKER_IMAGE
   docker push $DOCKER_IMAGE
 fi
 
-test -f secret.yml && kubectl apply -f secret.yml
+if [ -f "secret.yml" ]; then
+  kubectl delete -f secret.yml || true
+  kubectl apply -f secret.yml
+fi
 
 cat k8s.yml | envsubst | kubectl apply -f -
