5fa8e4711b445756a79f4883d4d4539435a8334f
Jonathan Demers <jdemers@opcodesolutions.com>
2023-01-08 08:54:30 -0500

    Access docker-registry directly (not through apache)

--- build-single.sh
+++ build-single.sh
@@ -14,7 +14,7 @@ cd $(dirname "$0")/$name
 if [ -f "Dockerfile" ]; then
   docker_name=com.opcode.k8s-infra.$name
   docker build --tag $docker_name .
-  export DOCKER_IMAGE=docker-registry.k8s.lan/$docker_name:latest-$(date "+%Y.%m.%d-%H.%M.%S")
+  export DOCKER_IMAGE=docker-registry.k8s.lan:30501/$docker_name:latest-$(date "+%Y.%m.%d-%H.%M.%S")
 #  docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW http://debian-cloud-test:8083
   docker tag $docker_name $DOCKER_IMAGE
   docker push $DOCKER_IMAGE
