e6b09981aa55220c9f754d6de31906330ee9d4c9
Jonathan Demers <jdemers@opcodesolutions.com>
2023-03-01 06:42:34 -0500

    Auto-generate Quick Links page

---
+++ apache/start-apache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+for website in $(cat /etc/apache2/sites-enabled/* | grep -E '^ +(ServerName|ServerAlias) ' | awk '{print $2}' | envsubst); do
+  echo "<a href='https://$website/'>$website</a><br/>"
+done >> /var/www/ROOT_DOMAIN_02/index.html 
+
+apachectl -D FOREGROUND
