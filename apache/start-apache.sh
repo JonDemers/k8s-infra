6f65c63976b9504a60f7b9976917fe24896eba40
Jonathan Demers <jdemers@opcodesolutions.com>
2023-10-01 12:23:23 -0400

    Remove trailing spaces

--- apache/start-apache.sh
+++ apache/start-apache.sh
@@ -4,6 +4,6 @@ set -e
 
 for website in $(cat /etc/apache2/sites-enabled/* | grep -E '^ +(ServerName|ServerAlias) ' | awk '{print $2}' | envsubst); do
   echo "<a href='https://$website/'>$website</a><br/>"
-done >> /var/www/ROOT_DOMAIN_02/index.html 
+done >> /var/www/ROOT_DOMAIN_02/index.html
 
 apachectl -D FOREGROUND
